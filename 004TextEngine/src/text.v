110dc738952156e1f3c8b017d5898f357ae4a6e9
Franco <Franco.Polo@mimi.io>
2024-04-15 13:35:29 +0200

    [004TextEngine] Displaying Specific Characters

--- 004TextEngine/src/text.v
+++ 004TextEngine/src/text.v
@@ -1,3 +1,39 @@
+module textRow #(
+    parameter ADDRESS_OFFSET = 8'd0
+) (
+    input clk,
+    input [7:0] readAddress,
+    output [7:0] outByte
+);
+    reg [7:0] textBuffer [15:0];
+
+    integer i;
+    // initial begin
+    //     for (i=0; i<15; i=i+1) begin
+    //         textBuffer[i] = 0;
+    //     end
+    //     textBuffer[0] = "L";
+    //     textBuffer[1] = "u";
+    //     textBuffer[2] = "s";
+    //     textBuffer[3] = "h";
+    //     textBuffer[4] = "a";
+    //     textBuffer[5] = "y";
+    //     textBuffer[6] = " ";
+    //     textBuffer[7] = "L";
+    //     textBuffer[8] = "a";
+    //     textBuffer[9] = "b";
+    //     textBuffer[10] = "s";
+    //     textBuffer[11] = "!";
+    // end
+    initial begin
+        for (i=0; i<15; i=i+1) begin
+            textBuffer[i] = 48 + ADDRESS_OFFSET + i;
+        end
+    end
+
+    assign outByte = textBuffer[(readAddress-ADDRESS_OFFSET)];
+endmodule
+
 module textEngine (
     input clk,
     input [9:0] pixelAddress,
@@ -35,7 +71,35 @@ module textEngine (
         outputBuffer <= fontBuffer[((chosenChar-8'd32) << 4) + (columnAddress << 1) + (topRow ? 0 : 1)];
     end
 
-    // The only thing missing is to know which character to output, but for now if we just add:
-    assign charOutput = "A"; 
+    // // The only thing missing is to know which character to output, but for now if we just add:
+    // assign charOutput = "A"; 
+
+    //
+    //
+    //
+    wire [7:0] charOutput1, charOutput2, charOutput3, charOutput4;
+
+    textRow #(6'd0) t1(
+        clk,
+        charAddress,
+        charOutput1
+    );
+    textRow #(6'd16) t2(
+        clk,
+        charAddress,
+        charOutput2
+    );
+    textRow #(6'd32) t3(
+        clk,
+        charAddress,
+        charOutput3
+    );
+    textRow #(6'd48) t4(
+        clk,
+        charAddress,
+        charOutput4
+    );
+
+    assign charOutput = (charAddress[5] && charAddress[4]) ? charOutput4 : ((charAddress[5]) ? charOutput3 : ((charAddress[4]) ? charOutput2 : charOutput1));
 
 endmodule
